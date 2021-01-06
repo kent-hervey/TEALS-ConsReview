c70dc92579cf5641489ef6f301ccd724255b1370
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-01-06 16:50:44 -0600

    Tested the total and starred  methods in XReview.java

--- src/hervey/mypackage/ReviewApp.java
+++ src/hervey/mypackage/ReviewApp.java
@@ -5,8 +5,12 @@ public class ReviewApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		double overaAllSentimentSimpleReview = XReview.totalSentiment("SimpleReview.txt");
+		System.out.println("overall review of SimpleReview is:  " + overaAllSentimentSimpleReview);
 		
-
+		double starRatingSimpleReview = XReview.starRating("SimpleReview.txt");
+		System.out.println("Star rating of SimpleReview is:  " + starRatingSimpleReview);
+		
 	}
 
 }
